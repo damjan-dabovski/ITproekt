4813a8e6dc9e48c293453393bf62e5d80db793f7
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2020-07-15 18:47:20 +0200

    Initial commit

---
+++ ITproekt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITproekt.MvcApplication" Language="C#" %>
---
+++ ITproekt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITproekt.Startup))]
+namespace ITproekt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
