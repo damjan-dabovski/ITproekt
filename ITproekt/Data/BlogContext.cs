f4069fec7aaacd36ac8eb46d53a47b0129181b62
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2020-07-15 20:31:45 +0200

    Scaffolding and migration for Product and DbContext

---
+++ ITproekt/Data/BlogContext.cs
@@ -0,0 +1,13 @@
+﻿using ITproekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ITproekt.Data {
+    public class BlogContext: DbContext {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
