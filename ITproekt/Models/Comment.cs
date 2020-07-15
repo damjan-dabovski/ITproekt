c6e8c624416b2fe42d734630cad64178584fd338
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2020-07-15 18:56:49 +0200

    Initial scaffolding for Post and Comment models

---
+++ ITproekt/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ITproekt.Models {
+    public class Comment {
+        public int Id { get; set; }
+        public string AuthorName { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ ITproekt/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITproekt.Models {
+    public class Post {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+
+        public Post() {
+            DateModified = DateCreated = DateTime.UtcNow;
+        }
+
+        public Post Update(Post newVersion) {
+            Title = newVersion.Title;
+            Content = newVersion.Content;
+            DateModified = DateTime.UtcNow;
+            return this;
+        }
+    }
+}
