25157763a9b5feb6ef3f8dd53fbd5bc86565ed7f
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2020-07-16 20:32:56 +0200

    Added UserController Index scaffolding

---
+++ ITproekt/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using ITproekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITproekt.Controllers
+{
+    public class UsersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+    }
+}
