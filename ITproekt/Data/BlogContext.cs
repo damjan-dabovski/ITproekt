5f1132e2362e68efb1a75f978891826f929b2494
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2020-07-16 14:02:01 +0200

    Properly applied the migrations and fixed the DbContext

--- ITproekt/Data/BlogContext.cs
+++ ITproekt/Data/BlogContext.cs
@@ -6,6 +6,9 @@ using System.Linq;
 
 namespace ITproekt.Data {
     public class BlogContext: DbContext {
+
+        public BlogContext() : base() { }
+
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Product> Products { get; set; }
--- ITproekt/Migrations/Configuration.cs
+++ ITproekt/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace ITproekt.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ITproekt.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ITproekt.Data.BlogContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(ITproekt.Models.ApplicationDbContext context)
+        protected override void Seed(ITproekt.Data.BlogContext context)
         {
             //  This method will be called after migrating to the latest version.
 
