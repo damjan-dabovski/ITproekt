24985e14666a8f9fd1c98d71f8a7196f45b6e6e2
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2020-07-21 17:50:58 +0200

    Applied migration for Post/Product extensions

--- ITproekt/Models/Comment.cs
+++ ITproekt/Models/Comment.cs
@@ -4,9 +4,13 @@ using System.ComponentModel.DataAnnotations;
 namespace ITproekt.Models {
     public class Comment {
         public int ID { get; set; }
+
+        [Required]
         public string AuthorName { get; set; }
+
         [DataType(DataType.MultilineText)]
         public string Content { get; set; }
+
         public DateTime DateCreated { get; set; }
 
         public int PostID { get; set; }
